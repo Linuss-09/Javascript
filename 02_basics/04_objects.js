e64801a44fe323ddb35863fd74df8afa471f1f5a
Linuss <aryan2003khare@gmail.com>
2025-05-18 19:44:29 +0000

    json k baare me padha humne

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,32 @@
+const course = {
+    coursename:"js in hindi",
+    price: '999' ,
+    courseInstructor: "aryan"
+}
+
+// course.courseInstructor  
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor)
+
+console.log(instructor);
+
+const navbar = ({company}) => {
+    console.log("hii");
+}
+
+navbar(company = "aryan_09")
+
+// API
+// {
+//     "name": "hitesh",
+//     "courename" : "js in hindi",
+//     "price" : "free",
+// }
+
+
+
+[
+    
+]
